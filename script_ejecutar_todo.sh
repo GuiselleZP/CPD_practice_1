81a32f696a342281f34623db7a9d62d03879e636
SebWarr <sebguerrerosal@unal.edu.co>
2020-04-01 12:56:38 -0500

    execution script completed

--- script_ejecutar_todo.sh
+++ script_ejecutar_todo.sh
@@ -1,12 +1,6 @@
-#g++ `pkg-config --cflags opencv` blur-effect-sec.cpp `pkg-config --libs opencv` -o blurSecuencial.out -lpthread
-#g++ `pkg-config --cflags opencv` blur-effect-par.cpp `pkg-config --libs opencv` -o blurParalelo.out -lpthread
 #! /bin/bash
 
-
-#! /bin/bash
-
-gcc -Wall -pedantic blur_effect_Sec.c -o blurSec -lm
-gcc -Wall -pedantic blur_effect_Par.c -o blurPar -lm
+gcc -pthread -Wall -pedantic blur_effect.c -o blur -lm
 
 
 echo "" > resultados.txt
@@ -19,19 +13,20 @@ echo "----------------------------------" >> resultados.txt
 echo "**********************************" >> resultados.txt
 
 kernel=3
-
+threads=1
 echo "----------------------------------" >> resultados.txt
 echo "         imagen 720p              " >> resultados.txt
 echo "----------------------------------" >> resultados.txt
 while [ $kernel -lt 16 ];
     do
     echo "Con un kernel de" $kernel >> resultados.txt
-    { time ./blurSec image1.jpg $kernel >/dev/null 2>&1; } 2>> resultados.txt
+    { time ./blur lobo720.jpg lobo720blur.jpg $kernel $threads >/dev/null 2>&1; } 2>> resultados.txt
     echo "" >> resultados.txt
     kernel=$[$kernel+2]
 done
 
 kernel=3
+threads=1
 echo " "
 echo "----------------------------------" >> resultados.txt
 echo "          imagen 1080p            " >> resultados.txt
@@ -39,12 +34,13 @@ echo "----------------------------------" >> resultados.txt
 while [ $kernel -lt 16 ];
     do
     echo "Con un kernel de" $kernel >> resultados.txt
-    { time ./blurSec image2.jpg $kernel >/dev/null 2>&1; } 2>> resultados.txt
+    { time ./blur lobo1080.jpg lobo1080blur.jpg $kernel $threads >/dev/null 2>&1; } 2>> resultados.txt
     echo "" >> resultados.txt
     kernel=$[$kernel+2]
 done
 
 kernel=3
+threads=1
 echo " "
 echo "----------------------------------" >> resultados.txt
 echo "          imagen 4k               " >> resultados.txt
@@ -52,7 +48,7 @@ echo "----------------------------------" >> resultados.txt
 while [ $kernel -lt 16 ];
     do
     echo "Con un kernel de" $kernel >> resultados.txt
-    { time ./blurSec image3.jpg $kernel >/dev/null 2>&1; } 2>> resultados.txt
+    { time ./blur lobo4k.jpg lobo4kblur.jpg $kernel $threads >/dev/null 2>&1; } 2>> resultados.txt
     echo "" >> resultados.txt
     kernel=$[$kernel+2]
 done
@@ -85,7 +81,7 @@ while [ $kernel -lt 16 ];
         do
         echo "Con un numero de hilos de" $threads >> resultados.txt
         echo "Con un kernel de" $kernel >> resultados.txt
-        { time ./blurPar image1.jpg $kernel $threads >/dev/null 2>&1; } 2>> resultados.txt
+        { time ./blur lobo720.jpg lobo720blur.jpg $kernel $threads >/dev/null 2>&1; } 2>> resultados.txt
 	echo "" >> resultados.txt
 	echo "" >> resultados.txt
 	threads=$[$threads*2]
@@ -106,7 +102,7 @@ while [ $kernel -lt 16 ];
         do
         echo "Con un numero de hilos de" $threads >> resultados.txt
         echo "Con un kernel de" $kernel >> resultados.txt
-        { time ./blurPar image2.jpg $kernel $threads >/dev/null 2>&1; } 2>> resultados.txt
+        { time ./blur lobo1080.jpg lobo1080blur.jpg $kernel $threads >/dev/null 2>&1; } 2>> resultados.txt
 	echo "" >> resultados.txt
 	echo "" >> resultados.txt
 	threads=$[$threads*2]
@@ -127,7 +123,7 @@ while [ $kernel -lt 16 ];
         do
         echo "Con un numero de hilos de" $threads >> resultados.txt
         echo "Con un kernel de" $kernel >> resultados.txt
-        { time ./blurPar image3.jpg $kernel $threads >/dev/null 2>&1; } 2>> resultados.txt
+        { time ./blur lobo4k.jpg lobo4kblur.jpg $kernel $threads >/dev/null 2>&1; } 2>> resultados.txt
 	echo "" >> resultados.txt
 	echo "" >> resultados.txt
 	threads=$[$threads*2]
